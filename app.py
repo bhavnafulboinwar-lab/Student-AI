5b45ec9ebaa4f65f24c5555d42163a0f53ebe958
Bhavna Fulboinwar <bhavnafulboinwar@gmail.com>
2026-06-28 13:24:44 +0530

    Deploy latest project

--- app.py
+++ app.py
@@ -1,42 +1,53 @@
 from flask import Flask, render_template, request
 import joblib
-import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
-# model = joblib.load("model/student_model.pkl")
+# Load the trained pipeline
+model = joblib.load("model/student_model.pkl")
+
 
 @app.route("/")
-def dashboard():
+def home():
     return render_template("dashboard.html")
+
 
 @app.route("/predict")
 def predict():
     return render_template("predict.html")
 
+
 @app.route("/result", methods=["POST"])
 def result():
 
-    attendance = float(request.form["attendance"])
-    study_hours = float(request.form["study_hours"])
-    previous_score = float(request.form["previous_score"])
+    gender = request.form["gender"]
+    race = request.form["race_ethnicity"]
+    parent = request.form["parental_level_of_education"]
+    lunch = request.form["lunch"]
+    course = request.form["test_preparation_course"]
+    reading = float(request.form["reading_score"])
+    writing = float(request.form["writing_score"])
 
-    if attendance >= 80 and study_hours >= 4 and previous_score >= 75:
-        prediction = "Excellent"
+    input_df = pd.DataFrame({
+        "gender": [gender],
+        "race_ethnicity": [race],
+        "parental_level_of_education": [parent],
+        "lunch": [lunch],
+        "test_preparation_course": [course],
+        "reading_score": [reading],
+        "writting_score":[writing]
+    })
 
-    elif attendance >= 60 and study_hours >= 2 and previous_score >= 50:
-        prediction = "Good"
-
-    elif attendance >= 40 and study_hours >= 1 and previous_score >= 35:
-        prediction = "Average"
-
-    else:
-        prediction = "Poor"
+    prediction = model.predict(input_df)[0]
 
     return render_template(
         "result.html",
-        prediction=prediction
+        prediction=round(prediction, 2)
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
